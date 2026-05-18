0613e895e6d0d1ec9782186ca7f5bfb543339e96
lbwright22ci <lbwright22@gmail.com>
2026-05-18 15:55:15 +0100

    customised admin

--- products/models.py
+++ products/models.py
@@ -7,6 +7,9 @@ class Category(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
+    class Meta:
+        verbose_name_plural = "Categories"
+        
     def __str__(self):
         return self.name
 
