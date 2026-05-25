ef5816b54fed8580e0b6feaa7e327fdcd3a45857
lbwright22ci <lbwright22@gmail.com>
2026-05-25 12:49:19 +0100

    enable toasts to give customer feedback

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -111,6 +111,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
