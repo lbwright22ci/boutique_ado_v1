6d5b0435e609c885f532e743dfaf9645bca66a7b
lbwright22ci <lbwright22@gmail.com>
2026-06-03 15:59:53 +0100

    enable confirmation emails to be sent in the back end

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+
 from .models import Order, OrderLineItem
 from products.models import Product
 from profiles.models import UserProfile
@@ -12,6 +16,26 @@ class StripeWH_Handler:
     def __init__(self, request):
         self.request = request
     
+    def _send_confirmation_email(self, order):
+        """ send the user an email when payment completed for their order"""
+        cust_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order }
+        )
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order,
+             'contact_email': settings.DEFAULT_FROM_EMAIL}
+        )
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )
+
+
     def handle_event(self, event):
         """ Handle generic/unknown/unexpected webhook event """
         return HttpResponse(
@@ -85,6 +109,7 @@ class StripeWH_Handler:
                 time.sleep(1)
             
         if order_exists:
+                self._send_confirmation_email(order)
                 return HttpResponse(
                     content =f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                     status = 200)
@@ -132,7 +157,7 @@ class StripeWH_Handler:
                     content=f'Webhook receieved: {event['type']} | ERROR {e}',
                     status=500
                 )
-
+        self._send_confirmation_email(order)
         return HttpResponse(
             content=f'Webhook receieved: {event['type']} | SUCCESS: created order in webhook',
             status=200)
