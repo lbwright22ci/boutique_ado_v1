0dc09bb8b02f5eee45a5801b47efc34638db27ee
lbwright22ci <lbwright22@gmail.com>
2026-06-04 16:09:18 +0100

    add missing slash to media url

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -179,6 +179,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
 
+
 if "USE_AWS" in os.environ:
     # Cache static files
     AWS_S3_OBJECT_PARAMETERS ={
@@ -197,8 +198,8 @@ if "USE_AWS" in os.environ:
     MEDIAFILES_STORAGE= 'custom_storage.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
     # Override static and media URL in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
