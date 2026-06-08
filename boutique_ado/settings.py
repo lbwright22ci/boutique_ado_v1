bda87179ad6450db55270955d02dc114d87bb5fe
lbwright22ci <lbwright22@gmail.com>
2026-06-08 13:12:26 +0100

    change python

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -205,8 +205,6 @@ if "USE_AWS" in os.environ:
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-
-
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
