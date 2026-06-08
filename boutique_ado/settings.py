3f515bea2fa8c4c3c930f3ab3c4fd293c51c2ebc
lbwright22ci <lbwright22@gmail.com>
2026-06-08 11:41:47 +0100

    adjust styling for mobile on home and product detail pages

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
+#
 DEBUG = 'DEVELOPMENT' in os.environ
+
 
 ALLOWED_HOSTS = [
     '127.0.0.1', #vs code preview
@@ -150,12 +152,12 @@ if 'D' in os.environ:
     DEFAULT_FROM_EMAIL='boutiqueado@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL= os.environ.get('boutiqueado@example.com')
+    DEFAULT_FROM_EMAIL= os.environ.get('EMAIL_HOST_USER')
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
